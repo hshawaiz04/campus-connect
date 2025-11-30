e9769828850276eddee3ae7a3de68f67b96798b3
Shawaiz Haider <shawaizhaider@gmail.com>
2025-11-30 09:48:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8510163853-ad51f",
+  "appId": "1:171686372080:web:dd933d6b986b3d4aeb9835",
+  "apiKey": "AIzaSyCg4xtrVt16sWnNauZVh3LkY85Q_R-iLQk",
+  "authDomain": "studio-8510163853-ad51f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "171686372080"
+};
